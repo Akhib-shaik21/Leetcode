e8eb76713efb85901dc5d5e90de368027e7a3bcb
Akhib Shaik <shaikakhib111892@gmail.com>
2025-10-11 21:48:32 +0530

    Time: 15 ms (18.45%) | Memory: 45 MB (27.68%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,17 +3,8 @@ class Solution {
         s=s.toLowerCase();
         s=s.replaceAll("[^a-z0-9]","");
         StringBuilder sb=new StringBuilder(s);
-        int low=0;
-        int high=sb.length()-1;
-        while(low<high)
-        {
-            char temp=sb.charAt(low);
-            sb.setCharAt(low,sb.charAt(high));
-            sb.setCharAt(high,temp);
-            low++;
-            high--;
-        }
-        String rev=sb.toString();
+        
+        String rev=sb.reverse().toString();
         if(rev.equals(s))
         {
             return true;
